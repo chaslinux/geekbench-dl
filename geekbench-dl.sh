4c5ec2bcd942519c46927ba5e642d40a0639458d
Chaslinux <chaslinux@gmail.com>
2023-06-02 10:44:32 -0400

    Initial commit of geekbench-dl script.

---
+++ geekbench-dl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# geekbench-dl.sh
+# by chaslinux, chaslinux@gmail.com
+
+# Download and install geekbench, run test
+
+# Constants
+GBVER=Geekbench-6.0.3-Linux.tar.gz
+GBDIR=Geekbench-6.0.3-Linux
+
+mkdir -p /home/$USER/Desktop/geekbench
+cd /home/$USER/Desktop/geekbench
+wget https://cdn.geekbench.com/$GBVER
+tar -zxvf $GBVER
+cd $GBDIR
+./geekbench6
