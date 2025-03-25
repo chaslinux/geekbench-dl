b0638a003171dd38bb5f321d11fa5811c0b7cb82
chaslinux <chaslinux@gmail.com>
2025-03-25 15:08:38 -0400

    Check to determine if it's an Intel or AMD CPU. If AMD run geekbench_x86_64, if Intel run geekbench6, which runs geekbench_avx2.

--- geekbench-dl.sh
+++ geekbench-dl.sh
@@ -8,13 +8,22 @@
 # Constants
 GBVER=Geekbench-6.4.0-Linux.tar.gz
 GBDIR=Geekbench-6.4.0-Linux
+CPUTEST=$(cat /proc/cpuinfo | grep 'model name' | cut -c 14- | cut -c -3 | head -n 1)
 
 mkdir -p /home/$USER/Desktop/geekbench
 cd /home/$USER/Desktop/geekbench
 wget https://cdn.geekbench.com/$GBVER
 tar -zxvf $GBVER
 cd $GBDIR
-./geekbench6
+
+if [ $CPUTEST=="AMD" ]
+	then
+		echo "This is an AMD CPU running geekbench_x86_64"
+		./geekbench_x86_64
+	else
+		echo "This is an Intel, or other processor, running geekbench_avx2"
+		./geekbench6
+fi
 
 # Remove the directory after running
 cd ..
